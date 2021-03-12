5ca0238a0bcb61668c5f924e10e2938d7988ba66
Daniel Kim <dgkim@ucdavis.edu>
2021-03-12 13:05:35 -0800

    added

--- pages/learn/index.js
+++ pages/learn/index.js
@@ -8,75 +8,14 @@ import { HttpLink } from 'apollo-link-http';
 import ChakraUIRenderer from 'chakra-ui-markdown-renderer';
 import ReactMarkdown from 'react-markdown'
 import { Box, Circle, Flex, Stack, useColorModeValue as mode } from '@chakra-ui/react'
+import { Cabin } from '@components/hero/cabin'
 
-export default function Learn({ menu }) {
-  console.log(menu)
+export default function Learn() {
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <Shell>
-        <StepHead />
-        <Box p="8">
-        {/* <ReactMarkdown
-          renderers={ChakraUIRenderer()}
-          source={readmedoc}
-          escapeHtml={false}
-        />; */}
-        </Box>
-      </Shell>
+      <Cabin />
     </Layout>
   )
 }
 
 
-
-
-export async function getServerSideProps(context) {
-  const token = "69380bc53bae0c1b608bd313d2c6042d1c0ed956";
-
-  const authLink = setContext((_, { headers }) => {
-    return {
-      headers: {
-        ...headers,
-        authorization: token ? `Bearer ${token}` : null,
-      }
-    }
-  });
-
-  const client = new ApolloClient({
-    link: authLink.concat(new HttpLink({ uri: 'https://api.github.com/graphql' })),
-    cache: new InMemoryCache()
-  });
-
-  const { data } = await client.query({
-    query: gql`
-        query {
-          repository(owner: "bitprj", name: "cabin") {
-            object(expression: "main:.bit/responses") {
-              ... on Tree {
-                entries {
-                  oid
-                  name
-                  type
-                  object {
-                    ... on Blob {
-                      text
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-        
-    `
-  });
-
-  return {
-    props: {
-      menu: data.repository.object.entries
-    }, // will be passed to the page component as props
-  }
-}
