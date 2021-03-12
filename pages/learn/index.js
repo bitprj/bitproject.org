c0100bf06b35f77640cd0c5d2675011de0531644
emsesc <emseschen@gmail.com>
2021-03-12 18:49:47 -0500

    Add protected feature

--- pages/learn/index.js
+++ pages/learn/index.js
@@ -9,8 +9,31 @@ import ChakraUIRenderer from 'chakra-ui-markdown-renderer';
 import ReactMarkdown from 'react-markdown'
 import { Box, Circle, Flex, Stack, useColorModeValue as mode } from '@chakra-ui/react'
 import { Cabin } from '@components/hero/cabin'
+import AccessDenied from '../components/access-denied'
+import { useState, useEffect } from 'react'
+import { useSession } from 'next-auth/client'
 
 export default function Learn() {
+  const [ session, loading ] = useSession()
+  const [ content , setContent ] = useState()
+
+  // Fetch content from protected route
+  useEffect(()=>{
+    const fetchData = async () => {
+      const res = await fetch('/api/protected')
+      const json = await res.json()
+      if (json.content) { setContent(json.content) }
+    }
+    fetchData()
+  },[session])
+
+  // When rendering client side don't display anything until loading is complete
+  if (typeof window !== 'undefined' && loading) return null
+
+  // If no session exists, display access denied message
+  if (!session) { return  <Layout><AccessDenied/></Layout> }
+
+  // If session exists, display content
   return (
     <Layout home>
       <Cabin />
