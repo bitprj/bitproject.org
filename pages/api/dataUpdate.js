0e3b2d5990f1f214b5a209edf636117cbbf440fa
emsesc <emseschen@gmail.com>
2021-03-13 11:50:06 -0500

    Update dataUpdate.js

--- pages/api/dataUpdate.js
+++ pages/api/dataUpdate.js
@@ -1,12 +1,11 @@
 import fetch from 'node-fetch'
-import { getSession } from 'next-auth/client'
+import { useSession } from 'next-auth/client'
 
 const HASURA_ENDPOINT = process.env.HASURA_ENDPOINT;
 const secret = process.env.HASURA_ADMIN_SECRET;
-
+const [ session, loading ] = useSession()
 
 export default async (req, res) => {
-    const session = await getSession({ req })
     const queryString = `
     mutation upsert_Github {
         insert_users_Github(objects: [{email: "${session.user.email}", name: "${session.user.name}"}], on_conflict: {constraint: Github_pkey, update_columns: [name]}) {
