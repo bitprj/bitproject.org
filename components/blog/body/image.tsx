409d92d58e62cedae42b2d1d6c51eeb16b2013d3
Daniel Kim <dgkim@ucdavis.edu>
2021-03-17 14:12:13 -0700

    Update image.tsx

--- components/blog/body/image.tsx
+++ components/blog/body/image.tsx
@@ -44,39 +44,5 @@ export default function Image({
     }
 
     // Or if you are using Cloudinary, it will move to here
-  } else {
-    // lazy load w/ a blurred placeholder of the image that's loading
-    if (lazy) {
-      return (
-        <div
-          ref={!supportsLazyLoading ? ref : undefined}
-          style={{
-            width: `${width}px`,
-            height: `${height}px`,
-            background: `no-repeat url(${blurredPlaceholderUrl(
-              publicId,
-              width,
-              height,
-            )})`,
-          }}
-        >
-          {inView ||
-            (supportsLazyLoading && (
-              <ChakraImage
-                src={generateImageUrl({
-                  delivery: { publicId },
-                  transformation: { ...transforms },
-                })}
-                loading="lazy"
-                width="100%"
-                {...props}
-              />
-            ))}
-        </div>
-      );
-    } else {
-      // Just render the image
-      return <ChakraImage src={url} width="100%" {...props} />;
-    }
-  }
+  } 
 }
