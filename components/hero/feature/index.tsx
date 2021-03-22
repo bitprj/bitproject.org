e320fb651205eca778cc2c79162f14fb74109e02
Daniel Kim <dgkim@ucdavis.edu>
2021-03-22 07:30:53 -0700

    Update index.tsx

--- components/hero/feature/index.tsx
+++ components/hero/feature/index.tsx
@@ -21,17 +21,14 @@ import {
           <Box mt="24">
             <SimpleGrid columns={{ base: 1, md: 2 }} spacing={{ base: '16', md: '8' }}>
               <Stack spacing="12" maxW="lg">
-                <SingleFeature icon={<Box as={FaCopy} w="6" h="6" />} title="Everyone is Welcome.">
-                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
-                  officia consequat duis enim velit mollit. Exercitation veniam consequat sunt.
+                <SingleFeature icon={<Box as={FaCopy} w="6" h="6" />} title="Calling all engineers">
+                Anyone with experience in the tech industry is welcome to apply. If you know the basics of software engineering and are passionate about sharing your knowledge and helping others, we want you! No previous mentorship experience required.  
                 </SingleFeature>
                 <SingleFeature icon={<Box as={FaPlug} w="6" h="6" />} title="Program Commitment">
-                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
-                  officia consequat duis enim velit mollit. Exercitation veniam consequat sunt.
+                Our Camps range from 4 to 8 weeks. Mentors and students will be paired at the start of the program and are required to meet weekly throughout its duration. This is an opportunity to help people aspiring to get into tech and build meaningful relationships.
                 </SingleFeature>
-                <SingleFeature icon={<Box as={FaRecycle} w="6" h="6" />} title="Session Guides">
-                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit
-                  officia consequat duis enim velit mollit. Exercitation veniam consequat sunt.
+                <SingleFeature icon={<Box as={FaRecycle} w="6" h="6" />} title="Scoped Mentorship">
+                Bit Project will provide a guide for each mentoring session to help keep meetings meaningful. For the first half of the program, these will focus on getting to know each other and discussing your interests and experiences in tech. In the second half, the focus will shift to students' final projects as you guide and work with them to build something awesome from scratch
                 </SingleFeature>
               </Stack>
               <Center shadow="lg" minH="26rem">
