5b9193f060b93738202b208f97640c398d3e465e
Daniel Kim <dgkim@ucdavis.edu>
2021-03-22 21:41:41 -0700

    added

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,25 @@ import { Hero } from '@components/hero'
 import { Landing } from '@components/hero/landing'
 import { Newsletter } from '@components/newsletter'
 import { TestimonialTwo } from '@components/testimonial/two'
+import { Feature } from '@components/hero/feature'
 
 export default function Home() {
+  const features =
+  [
+    {
+      desc : 'built for students, by students',
+      title : 'Built for students, by students',
+    },
+    {
+      desc : `ful. For the first half of the program, these will focus on getting to know each other and discussing your interests and experiences in tech. In the second half, the focus will shift to students' final projects as you guide and work with them to build something awesome from scratch`,
+      title : 'Built with the Industry',
+    },
+    {
+      desc : 'Our Camps range from 4 to 8 weeks. Mentors and students will be paired at the start of the program and are required to meet weekly throughout its duration. This is an opportunity to help people aspiring to get into tech and build meaningful relationships.',
+      title : 'Free & Accessible',
+    }
+  ]
+
   return (
     <Layout home>
       <Head>
@@ -30,7 +47,12 @@ export default function Home() {
         Image2="/headshots/stefanus.jpg"
         Quote2="Mentoring through Bit project has been a blast! Seeing the students work on projects from ideation to implementation has been super exciting."
       />
-      <Newsletter />
+      <Feature
+      Headline="We give students the tools they need to
+      launch their future in tech"
+      features={features}
+      Image="/coding.png"
+      />
     </Layout>
   )
 }
--- pages/volunteer.js
+++ pages/volunteer.js
@@ -25,8 +25,6 @@ export default function Volunteer() {
       }
     ]
 
-  console.log(features)
-
   return (
     <Layout home>
       <Head>
