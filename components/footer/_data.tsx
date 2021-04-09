67d61b6040d7a39a8e086e127c81edda06c92b08
Shreya <shrgupta@ucdavis.edu>
2021-04-08 20:25:58 -0700

    fixed footer links and updated logo to white logo

--- components/footer/_data.tsx
+++ components/footer/_data.tsx
@@ -37,9 +37,9 @@ export const links: LinkGroupData[] = [
   {
     title: 'Courses',
     links: [
-      { label: 'Serverless', href: '/courses/serverless' },
-      { label: 'DevOps School', href: '/courses/devops' },
-      { label: 'Data Science', href: '/courses/datascience' },
+      { label: 'Serverless', href: '/course/serverless' },
+      { label: 'DevOps School', href: '/course/devops' },
+      { label: 'Data Science', href: '/course/datascience' },
     ],
   },
 
