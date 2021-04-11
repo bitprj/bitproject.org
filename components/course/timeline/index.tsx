f563c3c1f394c0fb874ca43237d304c0f1591c14
Shreya <shrgupta@ucdavis.edu>
2021-04-11 01:28:14 -0700

    updated icons

--- components/course/timeline/index.tsx
+++ components/course/timeline/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
-import { AiOutlineBulb, AiOutlineSketch, AiOutlineGithub,AiOutlineVideoCamera } from 'react-icons/ai'
+import { FiPaperclip } from 'react-icons/fi';
+import { BiCodeBlock, BiPaperPlane } from 'react-icons/bi';
 import { Box, Icon, Link } from '@chakra-ui/react'
 import { List } from './List'
 import { ListItem } from './ListItem'
@@ -12,22 +13,22 @@ export const Timeline = () => {
         <List spacing="12">
           <Link href="https://apply.bitproject.org">
             <ListItem
-              title="April 30th Application Deadline"
+              title="April 30th: Application Deadline"
               subTitle="Submit your application to be matched with an industry mentor"
-              icon={<Icon as={AiOutlineVideoCamera} boxSize="6" />}
+              icon={<Icon as={FiPaperclip} boxSize="6" />}
             >
             </ListItem>
           </Link>
           <ListItem
             title="June 12th: Final Offers Sent Out"
             subTitle="Schedule your interview for the mentorship program"
-            icon={<Icon as={AiOutlineGithub} boxSize="6" />}
+            icon={<Icon as={BiPaperPlane} boxSize="6" />}
           >
           </ListItem>
           <ListItem
-            title="June 21st - August 16th"
+            title="June 21st - August 16th: Program"
             subTitle="Start learning and developing!"
-            icon={<Icon as={AiOutlineBulb} boxSize="6" />}
+            icon={<Icon as={BiCodeBlock} boxSize="6" />}
           >
           </ListItem>
         </List>
