fb18ae9a276531d424d020bd7ee78ee5995b60ca
Daniel Kim <dgkim@ucdavis.edu>
2021-04-17 13:06:40 -0700

    add

--- components/blog/body/typography.tsx
+++ components/blog/body/typography.tsx
@@ -1,37 +1,37 @@
 import { Heading, Text } from '@chakra-ui/react';
 
 export const H1 = ({ children }) => (
-  <Heading as="h1" size="xl" pb="1rem">
+  <Heading as="h1" size="xl" pb="1rem" pt="2rem" >
     {children}
   </Heading>
 );
 
 export const H2 = ({ children }) => (
-  <Heading as="h2" size="lg" pb="1rem">
+  <Heading as="h2" size="lg" pb="1rem"  pt="2rem" >
     {children}
   </Heading>
 );
 
 export const H3 = ({ children }) => (
-  <Heading as="h3" size="lg" pb="1rem">
+  <Heading as="h3" size="lg" pb="1rem"  pt="2rem" >
     {children}
   </Heading>
 );
 
 export const H4 = ({ children }) => (
-  <Heading as="h4" size="md" pb="1rem">
+  <Heading as="h4" size="md" pb="1rem" pt="2rem" >
     {children}
   </Heading>
 );
 
 export const H5 = ({ children }) => (
-  <Heading as="h5" size="md" pb="1rem">
+  <Heading as="h5" size="md" pb="1rem" pt="2rem" >
     {children}
   </Heading>
 );
 
 export const H6 = ({ children }) => (
-  <Heading as="h6" size="xl" pb="1rem">
+  <Heading as="h6" size="xl" pb="1rem" pt="2rem" >
     {children}
   </Heading>
 );
