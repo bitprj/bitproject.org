2513c2d98a53b029a939cd7fc80b4548ccb2edbe
Daniel Kim <dgkim@ucdavis.edu>
2021-04-17 13:51:00 -0700

    Update typography.tsx

--- components/blog/body/typography.tsx
+++ components/blog/body/typography.tsx
@@ -13,25 +13,25 @@ export const H2 = ({ children }) => (
 );
 
 export const H3 = ({ children }) => (
-  <Heading as="h3" size="lg" pb="1rem"  pt="2rem" >
+  <Heading as="h3" size="lg" pb="1rem"  pt="1rem" >
     {children}
   </Heading>
 );
 
 export const H4 = ({ children }) => (
-  <Heading as="h4" size="md" pb="1rem" pt="2rem" >
+  <Heading as="h4" size="md" pb="1rem" pt="1rem" >
     {children}
   </Heading>
 );
 
 export const H5 = ({ children }) => (
-  <Heading as="h5" size="md" pb="1rem" pt="2rem" >
+  <Heading as="h5" size="md" pb="1rem" pt="1rem" >
     {children}
   </Heading>
 );
 
 export const H6 = ({ children }) => (
-  <Heading as="h6" size="xl" pb="1rem" pt="2rem" >
+  <Heading as="h6" size="xl" pb="1rem" pt="1rem" >
     {children}
   </Heading>
 );
