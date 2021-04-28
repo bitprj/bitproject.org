c05f34938d44f4a0cc5e298b640d3848a5c27b87
Daniel Kim <dgkim@ucdavis.edu>
2021-04-27 23:37:48 -0700

    added hubspot

---
+++ components/hubspot.js
@@ -0,0 +1,5 @@
+export function Hubspot() {
+    return (
+            <script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/7218884.js"></script>
+    )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import customTheme from "@components/theme";
 import '@components/main.css'
+import { Hubspot } from '@components/hubspot'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={customTheme}>
+      <Hubspot />
       <Component {...pageProps} />
     </ChakraProvider>
   );
