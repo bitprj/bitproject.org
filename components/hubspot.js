22ed774e99d901ace5145553e866a94520249672
Daniel Kim <dgkim@ucdavis.edu>
2021-04-28 00:10:51 -0700

    add ganalytics

--- components/hubspot.js
+++ components/hubspot.js
@@ -1,5 +1,23 @@
 export function Hubspot() {
     return (
+            <head>
             <script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/7218884.js"></script>
+            <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+          </head>
     )
 }
