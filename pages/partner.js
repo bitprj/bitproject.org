b7bada1d420f1ab13cd7edc759042ac13eddfb06
jrumbawa <jomarumbawa@gmail.com>
2021-05-06 18:43:25 -0700

    add icons to partner features

--- pages/partner.js
+++ pages/partner.js
@@ -3,6 +3,7 @@ import Layout from '../components/layout'
 import { Landing } from '@components/hero/landing'
 import { TestimonialTwo } from '@components/testimonial/two'
 import { Feature } from '@components/hero/feature'
+import { FaBullhorn, FaCheck, FaHandshake } from 'react-icons/fa'
 import Meta from '@components/util/seo'
 
 export default function Partner() {
@@ -11,14 +12,17 @@ export default function Partner() {
     {
       desc : 'Anyone with experience in the tech industry is welcome to apply. If you know the basics of software engineering and are passionate about sharing your knowledge and helping others, we want you! No previous mentorship experience required.',
       title : 'Calling all engineers',
+      icon : <FaBullhorn />
     },
     {
       desc : `Bit Project will provide a guide for each mentoring session to help keep meetings meaningful. For the first half of the program, these will focus on getting to know each other and discussing your interests and experiences in tech. In the second half, the focus will shift to students' final projects as you guide and work with them to build something awesome from scratch`,
       title : 'Scoped Mentorship',
+      icon : <FaHandshake />
     },
     {
       desc : 'Our Camps range from 4 to 8 weeks. Mentors and students will be paired at the start of the program and are required to meet weekly throughout its duration. This is an opportunity to help people aspiring to get into tech and build meaningful relationships.',
       title : 'Program Commitment',
+      icon : <FaCheck />
     }
   ]  
 
